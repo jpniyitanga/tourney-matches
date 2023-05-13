f2a96aaea9d515cec09a30a55e2861ce462cc665
Jean Pierre Niyitanga <jpniyitanga@gmail.com>
2023-05-13 18:36:01 -0400

    Conditional Rendering

---
+++ src/components/MatchList.js
@@ -0,0 +1,14 @@
+import Match from "./Match";
+import matchData from "../data/matchData";
+
+function MatchList(props) {
+  const oneMatch = matchData[0];
+  return (
+    <section className="PlayerList MatchList">
+      <h1>Match list</h1>
+      <Match players={oneMatch.players} winner={oneMatch.winner} scoredifference={ oneMatch.scoreDifference} />
+    </section>
+  );
+}
+
+export default MatchList;
